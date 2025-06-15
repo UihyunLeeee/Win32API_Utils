333f7294feca317ac705668e70ce6739f777acd9
Uihyun.Lee <jangmoonwon@gmail.com>
2025-06-15 16:32:33 +0900

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>
+#include <stdio.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine, int nShowCmd)
+{
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASSW wc = { };
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassW(&wc))
+    {
+        MessageBoxW(NULL, L"Window Registration Failed!", L"Error", MB_ICONERROR | MB_OK);
+        return 0;
+    }
+
+    HWND hWnd = CreateWindowExW(
+        0,
+        CLASS_NAME,
+        L"My Simple Text Editor",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if (hWnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    int len;
+
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+    }
+
+    return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
