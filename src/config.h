5c507cca35fac4b7ca7631efdc1f03516c6943cf
Uihyun.Lee <jangmoonwon@gmail.com>
2025-07-18 11:31:52 +0900

    Release mode

--- src/config.h
+++ src/config.h
@@ -5,5 +5,5 @@
 // To compile in DEBUG mode, leave this line uncommented.
 // To compile in RELEASE mode, comment out this line.
 #include <windows.h> // Include Windows header for HANDLE and other types. 
-#define MY_APP_DEBUG_MODE
-//#define MY_APP_RELEASE_MODE
+//#define MY_APP_DEBUG_MODE
+#define MY_APP_RELEASE_MODE
